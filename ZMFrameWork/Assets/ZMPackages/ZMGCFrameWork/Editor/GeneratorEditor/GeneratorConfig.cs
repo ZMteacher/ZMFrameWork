a7e744f250d0abaf86dd8bad2ed3ff472ebd3f60
zm <zhumengxyedu@163.com>
2024-09-22 16:30:16 +0800

    修改脚本路径命名错误问题

--- ZMFrameWork/Assets/ZMPackages/ZMGCFrameWork/Editor/GeneratorEditor/GeneratorConfig.cs
+++ ZMFrameWork/Assets/ZMPackages/ZMGCFrameWork/Editor/GeneratorEditor/GeneratorConfig.cs
@@ -8,7 +8,7 @@ namespace ZM.GC
 {
     public class GeneratorConfig
     {
-        public static string FramworkPath = Application.dataPath + "/Scipts/";
+        public static string FramworkPath = Application.dataPath + "/Scripts/";
         public static string[] mModuleNameArr = new string[] { "HallWorld", "BattleWorld", "SKWorld", "WZWorld" };
         public static string[] mMudileNameSpaceArr = new[] { "ZMGC.Hall", "ZMGC.Battle", "ZMGC.SK", "ZMGC.WZ" };
         public static string[] folderArr = new string[] { "/DataMgr/", "/MsgMgr/", "/LogicCtrl/" };
